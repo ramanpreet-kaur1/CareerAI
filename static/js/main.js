4f88574e03469a9e98f2b4148a483609c58a60da
Ramanpreet Kaur <ramanaspal13@gmail.com>
2026-03-18 12:20:00 +0530

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -45,14 +45,3 @@ document.querySelectorAll('.feature-card, .step, .testimonial-card').forEach(el 
     observer.observe(el);
 });
 
-// Mobile menu toggle
-document.querySelector('.hamburger').addEventListener('click', function() {
-    document.querySelector('.mobile-menu').classList.toggle('active');
-});
-
-// Close menu when clicking link
-document.querySelectorAll('.mobile-menu a').forEach(link => {
-    link.addEventListener('click', () => {
-        document.querySelector('.mobile-menu').classList.remove('active');
-    });
-});
